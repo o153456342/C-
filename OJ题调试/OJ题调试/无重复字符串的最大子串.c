c6a6b847aa357af469259eadd3faae379ce078d6
Xiaoshu Kuang <o153456342@163.com>
2021-07-18 23:10:28 +0800

    日常练习

--- OJ题调试/OJ题调试/无重复字符串的最大子串.c
+++ OJ题调试/OJ题调试/无重复字符串的最大子串.c
@@ -1,30 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-int lengthOfLongestSubstring(char* s)
-{
-    int* arr = (int*)malloc(sizeof(int) * 26);
-    memset(arr, 0, sizeof(int) * 26);
-    int len = strlen(s);
-    int count = 0;
-
-    for (int i = 0; i < len; i++)
-    {
-        arr[s[i] - 'a']++;
-        if (arr[s[i] - 'a'] == 1)
-        {
-            count++;
-        }
-    }
-
-    return count;
-
-}
-
-int main()
-{
-    int a = lengthOfLongestSubstring("pwwkew");
-    printf("%d", a);
-    return 0;
-}
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <string.h>
+//
+//int lengthOfLongestSubstring(char* s)
+//{
+//    int* arr = (int*)malloc(sizeof(int) * 26);
+//    memset(arr, 0, sizeof(int) * 26);
+//    int len = strlen(s);
+//    int count = 0;
+//
+//    for (int i = 0; i < len; i++)
+//    {
+//        arr[s[i] - 'a']++;
+//        if (arr[s[i] - 'a'] == 1)
+//        {
+//            count++;
+//        }
+//    }
+//
+//    return count;
+//
+//}
+//
+//int main()
+//{
+//    int a = lengthOfLongestSubstring("pwwkew");
+//    printf("%d", a);
+//    return 0;
+//}
